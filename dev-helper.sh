5bd0209446ea186a4619637aacc81edb6760a0c9
Robert Stolarz <robert.stolarz@yandex.com>
2016-10-02 02:04:39 -0400

    Use watch instead of nodemon to run the process

--- dev-helper.sh
+++ dev-helper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "[dev-helper] Building and starting containers..."
 docker-compose -p lbrstorefront build
-nodemon --ext "js,html,css,json" --exec "docker-compose up" < /dev/null
+watch "docker-compose down && docker-compose up" --ignoreDotFiles
