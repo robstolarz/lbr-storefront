0fdf6aee9009ec2bf723f5d383b8687f2dbc62b2
Robert Stolarz <robert.stolarz@yandex.com>
2016-10-02 15:07:33 -0400

    Fix duplicated output from watcher
    TODO: Re-attach to docker logger when restarting container

--- dev-helper.sh
+++ dev-helper.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+function cleanup {
+	docker-compose down
+	exit
+}
 echo "[dev-helper] Building and starting containers..."
 docker-compose -p lbrstorefront build
-watch "docker-compose down && docker-compose up" --ignoreDotFiles
+trap cleanup EXIT INT
+docker-compose up -d
+watch "docker restart lbrstorefront_web-store_1" --ignoreDotFiles
