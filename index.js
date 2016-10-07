f2d7bcc4a7b0643c596e4de6f21f905a308428e8
Robert Stolarz <robert.stolarz@yandex.com>
2016-10-07 00:21:22 -0400

    Implement session storage prototype
    * Create session table when needed
    * Implement the "flurbo counter" demo application
    * Introduce a sample COOKIE_SECRET into the development environment

--- index.js
+++ index.js
@@ -25,16 +25,58 @@ function getTemplate(filename){
 	return promise;
 }
 
-var counter = 0;
 var serverPromise = Promise.resolve().then(function(){
+	var session = require('express-session');
+	app.use(session({
+		store: new (require('connect-pg-simple')(session))({
+			pg: require('pg').native || require('pg')
+		}),
+		secret: process.env.COOKIE_SECRET,
+		resave: false,
+		cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
+		saveUninitialized: false // so CloudFlare can cache the site!
+	}));
+}).then(function(){
+	// set up database
+	var pgp = require('pg-promise')({
+		// TODO: pg-native ?
+	});
+
+	var db = pgp({ // TODO: fork lib to use psql-type defaults
+		host: process.env.PGHOST,
+		database: process.env.PGDATABASE,
+		user: process.env.PGUSER,
+		password: process.env.PGPASSWORD
+	});
+	return db.query('SELECT EXISTS ( SELECT 1 FROM information_schema.tables WHERE table_name = \'session\' )').then(function(data){
+		var sessionTableExists = data[0].exists;
+		if(!sessionTableExists){
+			console.log("Session table didn't exist. Creating one now...");
+			return db.any(new pgp.QueryFile(path.join(__dirname,'node_modules/connect-pg-simple/table.sql')));
+		}
+	}).then(Promise.resolve(db));
+}).then(function(db){
 	return getTemplate(path.normalize('./templates/counter.html'))
 		.then(function(template){
 			app.get('/counter',function(req,res){
-				res.send(template({count:counter}));
+				var user = (req.session || {}).flurbos ? req.session : {id: "unregistered user", flurbos: 0};
+				res.send(template({user:user}));
 			});
-			app.post('/counter',function(req,res){
-				counter += parseInt(req.body.change);
-				res.redirect('/counter');
+			app.post('/api/changeCounter',function(req,res){
+				if(!req.session)
+					req.session.regenerate(fn);
+				else
+					fn();
+
+				function fn(){ // callbacks, man.
+					var flurbos = req.session.flurbos || 0;
+					req.session.flurbos = flurbos + parseInt(req.body.change);
+					res.redirect(req.header('Referer') || '/');
+				}
+			});
+			app.post('/api/logout',function(req,res){
+				req.session.destroy();
+				res.redirect(req.header('Referer') || '/');
 			});
 		});
 }).then(function(){
